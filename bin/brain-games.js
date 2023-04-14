92f36840c67ba4c6c3f9b17cb2c6cfdf68541391
Иван <Navi2022@yandex.ru>
2023-04-14 10:59:23 +0300

    .

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import cli from '../src/cli.js';
 
-const cli = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-};
-
-export default cli;
+cli();
